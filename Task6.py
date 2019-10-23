6ba209eacec8e87af374e4ff95d0d727f6b67220
Maks <nazirov.maksat96@gmail.com>
2019-10-23 15:34:33 +0600

    task6

---
+++ Task6.py
@@ -0,0 +1,20 @@
+# def bigger_guy(a1 , b2):
+#     if a1>b2:
+#         return a1
+#     else:
+#         return b2
+
+
+# print(bigger_guy(2 ,1))
+
+
+# def test_bigger_guy():
+#   assert bigger_guy(1, 2) == 2
+#   assert bigger_guy(10, 20) == 20
+#   assert bigger_guy(20, 10) == 20
+#   assert bigger_guy(10, 10) == 10
+#   assert bigger_guy(2, 1) == 2
+#   assert bigger_guy('a', 'b') == 'b' # 'b' is greater than 'a'
+#   print("YOUR CODE IS CORRECT!")
+# #test your code by un-commenting the line(s) below
+# test_bigger_guy()
